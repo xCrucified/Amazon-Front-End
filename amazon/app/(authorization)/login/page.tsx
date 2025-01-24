28ffb9a7fefe9f9420b83128cf5c67f439673446
andrxpie <venganpro12@gmail.com>
2025-01-25 01:43:46 +0200

    Updated logo usage.

--- amazon/app/(authorization)/login/page.tsx
+++ amazon/app/(authorization)/login/page.tsx
@@ -1,5 +1,5 @@
 import { LoginForm } from "@/components/shared/login-form";
-import Logo from "@/components/ui/logo";
+import Image from "next/image";
 import Link from "next/link";
 
 export default function LoginPage() {
@@ -7,7 +7,7 @@ export default function LoginPage() {
     <div className="flex min-h-svh flex-col items-center justify-center bg-muted p-[32px]">
       <div className="flex w-[405px] max-w-sm flex-col gap-6">
         <Link href="/" className="items-center self-center">
-          <Logo />
+          <Image src={"/assets/images/LogoFull.svg"} alt="logo" width={206} height={64} />
         </Link>
         <LoginForm /> 
       </div>
