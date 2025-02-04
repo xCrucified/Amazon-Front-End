118e6eb5c2781f9f3f1757a3be4fa4a9d6db3c29
17:17 <eexet17@gmail.com>
2025-02-05 01:19:37 +0200

    Implement ProductCard component with data fetching; add Axios dependency

--- amazon/components/shared/(Cards)/product-card.tsx
+++ amazon/components/shared/(Cards)/product-card.tsx
@@ -1,11 +1,41 @@
-// import React from "react";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 
+interface Product {
+  id: number;
+  name: string;
+  price: number;
+  image: string;
+}
 
-// export const ProductCard: React.FC<Props> = ({ id, name, price, imageUrl, className }) => {
-//   return (
-//     <>
-//     </>
-//   );
-// };
+interface Props {
+  className?: string;
+}
 
-// export default ProductCard;
+export const ProductCard: React.FC<Props> = ({ className }) => {
+  const [data, setData] = useState<Product[]>([]);
+
+  useEffect(() => {
+    axios
+      .get("http://localhost:5000/data")
+      .then((response) => setData(response.data))
+      .catch((error) => console.error("Ошибка:", error));
+  }, []);
+
+  return (
+    <div className={className}>
+      <h1>Best Sellers in Sports & Outdoors</h1>
+      <div className="product-list">
+        {data.map((item) => (
+          <div key={item.id} className="product-card">
+            <img src={item.image} alt={item.name} />
+            <h2>{item.name}</h2>
+            <p>Цена: ${item.price}</p>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default ProductCard;
