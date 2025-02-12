40f29aec43730314f30fb05aece9065736dd7905
xCrucified <eexet17@gmail.com>
2025-02-13 00:03:54 +0100

    Refactor Modal component; enhance layout and styling, update content structure

--- amazon/components/ui/modal.tsx
+++ amazon/components/ui/modal.tsx
@@ -12,29 +12,21 @@ export const Modal = ({
   if (!isOpen) return null;
 
   return (
-    // <div className="z-[51] fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center">
-    //   <div className="bg-white p-6 rounded-lg shadow-lg">
-    //     <h2 className="text-lg font-bold"></h2>
-    //     <p>Это контент модального окна.</p>
-    //     <Button
-    //       onClick={onClose}
-    //       className="mt-4 bg-blue-500 text-white px-4 py-2 rounded"
-    //     >
-    //       Закрыть
-    //     </Button>
-    //   </div>
-    // </div>
-    <Container className="z-[51] h-[740px] w-[1445px] absolute inset-[0] top-[150px] bg-white flex items-center justify-center">
-      <div className="bg-white p-6 rounded-lg shadow-lg">
-        <h2 className="text-lg font-bold"></h2>
-        <p>Это контент модального окна.</p>
-        <Button
-          onClick={onClose}
-          className="mt-4 bg-blue-500 text-white px-4 py-2 rounded"
-        >
-          Закрыть
-        </Button>
-      </div>
-    </Container>
+    <>
+      <div className="fixed inset-0 bg-black opacity-50 z-[51]"></div>
+
+      <Container className="h-[739px] w-[1447px] mt-[139px] z-[51] fixed inset-0 flex items-center justify-center bg-[#f5f5f5] shadow-[white] shadow-2xl rounded-xl">
+        <div className="relative w-[100%] h-[100%]">
+          <h2 className="text-lg font-bold">Caption</h2>
+          <p>Content.</p>
+          <Button
+            onClick={onClose}
+            className="mt-4 bg-blue-500 text-white px-4 py-2 rounded"
+          >
+            Close
+          </Button>
+        </div>
+      </Container>
+    </>
   );
 };
