c31cd5783738058ad1462d18b77e29a2248d3bd3
xCrucified <eexet17@gmail.com>
2025-02-13 09:45:19 +0100

    Enhance Modal component; add body overflow control on open/close and apply shadow styling

--- amazon/components/ui/modal.tsx
+++ amazon/components/ui/modal.tsx
@@ -1,4 +1,5 @@
-import React from "react";
+"use client";
+import React, { useEffect } from "react";
 import { Button } from "./button";
 import { Container } from "../shared/container";
 
@@ -9,13 +10,25 @@ export const Modal = ({
   isOpen: boolean;
   onClose: () => void;
 }) => {
+  useEffect(() => {
+    if (isOpen) {
+      document.body.style.overflow = "hidden";
+    } else {
+      document.body.style.overflow = "";
+    }
+
+    return () => {
+      document.body.style.overflow = "";
+    };
+  }, [isOpen]);
+
   if (!isOpen) return null;
 
   return (
     <>
-      <div className="fixed inset-0 bg-black opacity-50 z-[51]"></div>
+      <div className="fixed inset-0 bg-black opacity-25 h-[960px] overflow-hidden pr-[15px] flex justify-end self-end"></div>
 
-      <Container className="h-[739px] w-[1447px] mt-[139px] z-[51] fixed inset-0 flex items-center justify-center bg-[#f5f5f5] shadow-[white] shadow-2xl rounded-xl">
+      <Container className="h-[739px] w-[1447px] mt-[139px] z-[51] fixed inset-0 flex items-center justify-center bg-[#f5f5f5] modal-shadow rounded-bl-xl rounded-br-xl">
         <div className="relative w-[100%] h-[100%]">
           <h2 className="text-lg font-bold">Caption</h2>
           <p>Content.</p>
