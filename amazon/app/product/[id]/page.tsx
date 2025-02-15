816008f90ce1a4dc3df94ff1262984ea872ba0cf
xCrucified <eexet17@gmail.com>
2025-02-15 23:35:05 +0100

    Enhance ProductPage layout; add Label component for product title and adjust container styles

--- amazon/app/product/[id]/page.tsx
+++ amazon/app/product/[id]/page.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { cn } from "@/lib/utils";
 import { Container } from "@/components/shared/container";
+import { Label } from "@/components/ui/label";
 
 interface Props {
   className?: string;
@@ -11,12 +12,12 @@ export const ProductPage: React.FC<Props> = ({ className, params }) => {
   return (
     <Container>
       <div className={cn("flex gap-[56px] outline w-full h-full mt-[53px] justify-center items-center p-6", className)}>
-        <div className="outline w-[696px]">
+        <div className="w-[696px] rounded-md">
             <img src="/assets/images/productImg.png" alt="product" />
         </div>
 
-        <div className="outline w-[740px]">
-            {params.id}
+        <div className="outline w-[740px] h-full flex justify-start">
+            <Label className="text-[23px] font-bold w-[594px]">Canon EF 75-300mm f/4-5.6 III Telephoto Zoom Lens for Canon SLR Cameras</Label>
         </div>
       </div>
 
