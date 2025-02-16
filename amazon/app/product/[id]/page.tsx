c12ef073d3e55537cfe80d181fbda6836f274ce3
xCrucified <eexet17@gmail.com>
2025-02-16 11:55:52 +0100

    Refactor ProductPage layout; improve readability of product title and adjust component structure

--- amazon/app/product/[id]/page.tsx
+++ amazon/app/product/[id]/page.tsx
@@ -11,17 +11,23 @@ interface Props {
 export const ProductPage: React.FC<Props> = ({ className, params }) => {
   return (
     <Container>
-      <div className={cn("flex gap-[56px] outline w-full h-full mt-[53px] justify-center items-center p-6", className)}>
+      <div
+        className={cn(
+          "flex gap-[56px] outline w-full h-full mt-[53px] justify-center items-center p-6",
+          className
+        )}
+      >
         <div className="w-[696px] rounded-md">
-            <img src="/assets/images/productImg.png" alt="product" />
+          <img src="/assets/images/productImg.png" alt="product" />
         </div>
 
         <div className="outline w-[740px] h-full flex justify-start">
-            <Label className="text-[23px] font-bold w-[594px]">Canon EF 75-300mm f/4-5.6 III Telephoto Zoom Lens for Canon SLR Cameras</Label>
+          <Label className="text-[23px] font-bold w-[594px]">
+            Canon EF 75-300mm f/4-5.6 III Telephoto Zoom Lens for Canon SLR
+            Cameras
+          </Label>
         </div>
       </div>
-
-
     </Container>
   );
 };
