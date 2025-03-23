b604649c12f1fe4e5dca4def379773f07df30137
xCrucified <eexet17@gmail.com>
2025-03-23 12:25:52 +0100

    Enhance ListProducts component: add title, improve layout, and maintain product interaction functionality

--- amazon/components/shared/admin-list/ListProducts.tsx
+++ amazon/components/shared/admin-list/ListProducts.tsx
@@ -7,6 +7,7 @@ import { Label } from "@/components/ui/label";
 import StarRating from "@/components/ui/star-rating";
 import router from "next/dist/client/router";
 import Link from "next/link";
+import { Title } from "@/components/ui/title";
 
 interface Product {
   id: number;
@@ -68,77 +69,82 @@ const ListCategories = () => {
   if (loading) return <p>Loading categories...</p>;
 
   return (
-    <div className="flex w-full gap-10">
-      {products.map((product) => (
-        <div className={"relative w-[284px] h-[400px] mb-12"} key={product.id}>
-          <div className="bg-white rounded-2xl h-[100%] w-[100%]">
-            <div className="m-2.5">
-              <img
-                src={"/assets/images/products/mat.svg"}
-                alt={product.name}
-              ></img>
+    <>
+      <Title text={"Products"} size="lg" className="font-extrabold" />
+      <div className="flex w-full gap-10">
+        {products.map((product) => (
+          <div
+            className={"relative w-[284px] h-[400px] mb-12"}
+            key={product.id}
+          >
+            <div className="bg-white rounded-2xl h-[100%] w-[100%]">
+              <div className="m-2.5">
+                <img
+                  src={"/assets/images/products/mat.svg"}
+                  alt={product.name}
+                ></img>
 
-              <div>
-                <p className="text-sm text-[#757575]">Mats</p>
-                <Label className="text-[20px] font-bold max-w-[252px] flex flex-col">
-                  {product.name.length > 24
-                    ? `${product.name.slice(
-                        0,
-                        product.name.lastIndexOf(" ")
-                      )}...`
-                    : product.name}
-                </Label>
-              </div>
+                <div>
+                  <p className="text-sm text-[#757575]">Mats</p>
+                  <Label className="text-[20px] font-bold max-w-[252px] flex flex-col">
+                    {product.name.length > 24
+                      ? `${product.name.slice(
+                          0,
+                          product.name.lastIndexOf(" ")
+                        )}...`
+                      : product.name}
+                  </Label>
+                </div>
 
-              <div className="text-[#5a6b8c] gap-4 p-0">
-                <StarRating
-                  key={product.id}
-                  rate={5}
-                  secondHalf
-                  icon
-                ></StarRating>
-                <div className="flex gap-[10px]">
-                  <div>
-                    <span className="text-lg">£</span>
-                    <Label className="text-3xl font-bold w-[82.4px] h-[23px] ">
-                      {Number(product.price)}
-                    </Label>
-                  </div>
-                  <div>
-                    <Label className="text-base text-[#a2a5ab]">
-                      {product.oldPrice ? <del>£{product.oldPrice}</del> : null}
-                    </Label>
+                <div className="text-[#5a6b8c] gap-4 p-0">
+                  <StarRating
+                    key={product.id}
+                    rate={5}
+                    secondHalf
+                    icon
+                  ></StarRating>
+                  <div className="flex gap-[10px]">
+                    <div>
+                      <span className="text-lg">£</span>
+                      <Label className="text-3xl font-bold w-[82.4px] h-[23px] ">
+                        {Number(product.price)}
+                      </Label>
+                    </div>
+                    <div>
+                      <Label className="text-base text-[#a2a5ab]">
+                        {product.oldPrice ? (
+                          <del>£{product.oldPrice}</del>
+                        ) : null}
+                      </Label>
+                    </div>
                   </div>
                 </div>
               </div>
+              <div className="flex relative justify-between bottom-0">
+                <Button
+                  onClick={() => handleEdit(product.id)}
+                  className="bg-blue-400 hover:opacity-80"
+                >
+                  Edit
+                </Button>
+                <Link
+                  className="flex bg-green-600 hover:opacity-80 rounded-md text-white items-center p-1"
+                  href={`product/${product.id}`}
+                >
+                  Locate to product
+                </Link>
+                <Button
+                  onClick={() => handleDelete(product.id)}
+                  className="bg-red-400 hover:opacity-80"
+                >
+                  Remove
+                </Button>
+              </div>
             </div>
-            <div className="flex relative justify-between bottom-0">
-              <Button
-                onClick={() => handleEdit(product.id)}
-                className="bg-blue-400 hover:opacity-80"
-              >
-                Edit
-              </Button>
-              <Link
-                className="flex bg-green-600 hover:opacity-80 rounded-md text-white items-center p-1"
-                href={`product/${product.id}`}
-              >
-                Locate to product
-              </Link>
-              <Button
-                onClick={() => handleDelete(product.id)}
-                className="bg-red-400 hover:opacity-80"
-              >
-                Remove
-              </Button>
-            </div>
-            <Link className="flex mt-1 bg-gray-500 rounded-xl p-2 justify-center items-center text-white" href={`/profile/${product.userId}`}>
-              Locate to user
-            </Link>
           </div>
-        </div>
-      ))}
-    </div>
+        ))}
+      </div>
+    </>
   );
 };
 
